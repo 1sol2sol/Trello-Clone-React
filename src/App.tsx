ed9d6c955f3c5988da9ffb3816e9079bd2fbd0dd
1sol2sol <17gksthf@gmail.com>
2022-08-24 00:44:48 +0900

    Add styles and using map() to Draggable

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,60 @@
 import {DragDropContext, Draggable, Droppable} from "react-beautiful-dnd";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 480px;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+const Boards = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(1, 1fr); 
+`
+
+const Board = styled.div`
+  padding: 20px 10px;
+  padding-top: 30px;
+  background-color: ${props => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 200px;
+  `
+const Card = styled.div`
+  border-radius: 5px;
+  margin-bottom: 10px;
+  padding: 10px; 10px;
+  background-color: ${props => props.theme.cardColor}
+`
+
+const toDos = ["a","b","c","d","e","f"];
 
 function App() {
   const onDragEnd = () => {};
   return(
     <DragDropContext onDragEnd={onDragEnd}>
-      <div>
-        <Droppable droppableId="one">
-          {(provided) => 
-            <ul ref={provided.innerRef} {...provided.droppableProps}>
-              <Draggable draggableId="first" index={0}>
-                {(provided) => 
-                  <li ref={provided.innerRef} {...provided.draggableProps} >
-                    <span {...provided.dragHandleProps}>🍓</span>
-                    One 
-                  </li>
-                }
-              </Draggable>
-            </ul>
-          }
-        </Droppable>
-      </div>
+      <Wrapper>
+        <Boards>
+          <Droppable droppableId="one">
+            {(provided) => 
+              <Board ref={provided.innerRef} {...provided.droppableProps}>
+                {toDos.map((toDo, index) =><Draggable draggableId={toDo} index={index}>
+                  {(provided) => 
+                    <Card ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                      {toDo} 
+                    </Card>
+                  }
+                </Draggable>)}
+                {/* 끝나는 부분에 placeholder를 적용해서 아이템을 밖으로 움직여도 보드의 크기가 줄어들지 않도록 함  */}
+                {provided.placeholder} 
+              </Board>
+            }
+          </Droppable>
+        </Boards>
+      </Wrapper>
     </DragDropContext>
   )
 }
